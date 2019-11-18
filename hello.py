6608c08e2fe05ec74c247395f4eddc273524fd10
nadaeng <nadaeng.kr@gmail.com>
2019-11-18 21:29:32 +0900

    add func print_git

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
+import sys
+
 def print_message():
 	message = "world"
 	print("hello {}".format(message))
 
+def print_git():
+	message = "from the hell"
+	sys.print("git {}.format(message))
+
 if__name == "__main__":
 	print_message()
+	print_git()
